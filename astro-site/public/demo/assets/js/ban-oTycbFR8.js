edbd911882f03cc3a554e2234dda1c814ce7dd9a
Emir Yücel <emir@asistanapp.com.tr>
2025-12-10 17:00:47 +0300

    fix: remove manualChunks to resolve runtime dependency issues

---
+++ astro-site/public/demo/assets/js/ban-oTycbFR8.js
@@ -0,0 +1,7 @@
+import{c}from"./index-ByjBXa3D.js";
+/**
+ * @license lucide-react v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=c("ban",[["path",{d:"M4.929 4.929 19.07 19.071",key:"196cmz"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]]);export{a as B};
---
+++ astro-site/public/demo/assets/js/config-D68ZPno9.js
@@ -0,0 +1,1 @@
+const e=!0,s=300,o=(e=s)=>new Promise(s=>setTimeout(s,e)),t=()=>e;export{t as i,o as m};
